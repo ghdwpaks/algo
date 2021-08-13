c0dcf2323a8979ec6daabdd2c263c37d64869180
ghdwpaks <hjm7904@gmail.com>
2021-08-13 14:58:09 +0900

    sub 1 more shorty

--- 1sub/main.py
+++ 1sub/main.py
@@ -1,21 +1,13 @@
 import copy as c
 
-
-
 def selecting_move_target(selected) :
     global ban_list
     ban_list[selected] = 1
     print(selected,end=" ")
-    selected_list = c.deepcopy(table[selected])
-    for i in range(len(selected_list)) :
-        if selected_list[i] == 1 :
-            if ban_list[i] == 0 :
-                res = selecting_move_target(i)
-                if res == 999 :
-                    break
-        if i == len(selected_list)-1 :
-            break
-    return 999
+    for i in range(len(table[selected])) :
+        if table[selected][i] == 1 and ban_list[i] == 0 :
+                selecting_move_target(i)
+                break
         
 def printing_else(selected_list) :
     global ban_list
@@ -24,23 +16,21 @@ def printing_else(selected_list) :
             print(i,end=" ")
             pass
         
+while True :
 
+    table = [
+    [0,0,1,1,0,1],
+    [0,0,0,1,1,1],
+    [0,0,0,0,1,1],
+    [0,0,0,0,0,0],
+    [1,0,0,0,0,1],
+    [0,0,0,0,0,0]]
+    ban_list = [0,0,0,0,0,0]
+    selected = int(input("입력 :"))
 
-table = [
-[0,0,1,1,0,1],
-[0,0,0,1,1,1],
-[0,0,0,0,1,1],
-[0,0,0,0,0,0],
-[1,0,0,0,0,1],
-[0,0,0,0,0,0]]
-ban_list = []
-for i in range(len(table)) :
-    ban_list.append(0)
-
-selected = int(input("입력 :"))
-
-selecting_move_target(selected)
-printing_else(table[selected])
+    selecting_move_target(selected)
+    printing_else(table[selected])
+    print("\n\n")
 
     
 
