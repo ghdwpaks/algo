df7b6c4ce9aff0b3a106ca5aa15de41b193df02d
ghdwpaks <hjm7904@gmail.com>
2021-09-16 22:04:27 +0900

    get solved sub 6

--- 6sub/main.py
+++ 6sub/main.py
@@ -3,3 +3,22 @@ b = 0
 g = 0
 
 p = False
+while True :
+    r = round(r,2)
+    if r > 13 :
+        break
+    
+    b = 8 - r
+    g = 13 - r
+
+    if (g - r) == 6 and (g+r) == 13 :
+        p = True
+        break
+    
+    r += 0.01
+
+if p :
+    print("빨강은",r)
+    print("파랑은",b)
+    print("초록은",g)
+    
